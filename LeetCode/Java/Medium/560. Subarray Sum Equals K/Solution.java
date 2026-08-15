1f37842980bb9191a11c2befd817c1a4254ad187
Boddeti Musilinaidu <bmnaidu077@gmail.com>
2026-08-15 21:45:25 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/560. Subarray Sum Equals K/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+       //Using the Prefix Sum:
+       int count=0;
+       int prefix[] = new int [nums.length+1];
+       for(int i=0;i<nums.length;i++){
+        prefix[i+1]=prefix[i]+nums[i];
+       } 
+       for(int left=0;left<nums.length;left++){
+        for(int right=left;right<nums.length;right++){
+            int sum = prefix[right+1]-prefix[left];
+             if(sum==k){
+            count++;
+        }
+        }
+       
+       }
+       return count;
+    }
+}
