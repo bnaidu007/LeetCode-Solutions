aa6181a4ea3a92a2e643cca7bf4e46e5bbe11ce6
Boddeti Musilinaidu <bmnaidu077@gmail.com>
2026-08-17 10:53:22 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.java
+++ LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
+        if()
        int prefix[] = new int[nums.length+1];
        for(int i=0;i<nums.length;i++){
         prefix[i+1]=prefix[i]+nums[i];
