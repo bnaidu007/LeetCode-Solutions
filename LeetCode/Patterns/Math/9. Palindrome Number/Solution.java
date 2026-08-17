068e44d4ccf0f7671c0da3a2b7f8f4677891702b
Boddeti Musilinaidu <bmnaidu077@gmail.com>
2026-08-17 16:01:38 +0530

    Add Java Solution to Patterns/Math

--- LeetCode/Patterns/Math/9. Palindrome Number/Solution.java
+++ LeetCode/Patterns/Math/9. Palindrome Number/Solution.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        //two pointer method
+        // String st = String.valueOf(x);
+        // int left = 0;
+        // int right = st.length()-1;
+        // while(left<right){
+        //     if(st.charAt(left)!=(st.charAt(right))){
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+
+
+        //Using the number reverse
+        int or =x;
+        int re = 0;
+        while(x>0){
         
-        if (x<0){
-            return false;
-        }
-        String st = String.valueOf(x);
-
-        int left = 0;
-        int right = st.length()-1;
-
-        while(left<right){
-            if (st.charAt(left)!=st.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-
-        }
-        return true;
-
+        int digit = x%10;
+        re = re *10 +digit;
+        x=x/10;
+    }
+    return or==re;
     }
 }
