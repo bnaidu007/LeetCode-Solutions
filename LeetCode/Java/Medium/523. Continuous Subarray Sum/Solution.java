55a4b2e1e6a25107140d6916056e2bfaccd3cfb6
Boddeti Musilinaidu <bmnaidu077@gmail.com>
2026-08-17 10:53:38 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/523. Continuous Subarray Sum/Solution.java
+++ LeetCode/Java/Medium/523. Continuous Subarray Sum/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        if()
+        if(nums.length<2){
+            return flase;
+        }
        int prefix[] = new int[nums.length+1];
        for(int i=0;i<nums.length;i++){
         prefix[i+1]=prefix[i]+nums[i];
