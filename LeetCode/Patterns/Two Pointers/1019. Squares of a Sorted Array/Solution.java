4ae5b3d2571e9af537372c4eddec3318f86cb751
Boddeti Musilinaidu <bmnaidu077@gmail.com>
2026-08-19 13:21:18 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.java
+++ LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.java
@@ -1,10 +1,26 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int arr[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            arr[i] = nums[i]*nums[i];
+        // for(int i=0;i<nums.length;i++){
+        //     arr[i] = nums[i]*nums[i];
+        // }
+        // Arrays.sort(arr);
+        // return arr;
+        int left = 0;int right= nums.length-1;int k=nums.length-1;
+        while(left<=right){
+            int leftsq = nums[left]*nums[left];
+            int rightsq = nums[right]*nums[right];
+
+            if(leftsq>rightsq){
+                arr[k]=leftsq;
+                left++;
+            }
+            else{
+                arr[k] = rightsq;
+                right--;
+            }
+            k--;
         }
-        Arrays.sort(arr);
         return arr;
     }
 }
